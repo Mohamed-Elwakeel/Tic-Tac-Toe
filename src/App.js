029c4c97748175bb31fbca31ef2666a953c79255
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-09-03 21:37:54 +0300

    square and the board for the tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Board } from "./Components/Board/Board";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Board />
     </div>
   );
 }
